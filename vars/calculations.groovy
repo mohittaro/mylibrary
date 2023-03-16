40e49aabea99bcfcb1c7236eb3bede4a9f3204e3
prasadcloud <127221508+prasadcloud@users.noreply.github.com>
2023-03-16 22:38:05 +0530

    Create calculations.groovy

---
+++ vars/calculations.groovy
@@ -0,0 +1,16 @@
+def sum(a,b)
+{
+echo "The sum of ${a} and ${b} is ${a+b}"
+}
+def mult(a,b)
+{
+echo "The multiplication of ${a} and ${b} is ${a*b}"
+}
+def sub(a,b)
+{
+echo "The difference of ${a} and ${b} is ${a-b}"
+}
+def div(a,b)
+{
+echo "The division of ${a} and ${b} is ${a/b}"
+}
